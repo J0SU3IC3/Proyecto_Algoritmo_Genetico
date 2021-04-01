5407ecfcb2019558a42cbed89deac7c28b8b5906
J0su3 <zS17013400@estudiantes.uv.com>
2021-03-31 18:35:52 -0600

    Se noto al leer con cuidado el pdf que se representaria a los individuos como vectores de permutacion, por lo que se hizo una nuevafuncion que genere una poblacion como la que se pide,

--- Funcion generadora de matrices.py
+++ Funcion generadora de matrices.py
@@ -8,3 +8,28 @@ def matrices50(num_mat, fil, col):
             p = random.randrange(0, col)
             pob[n, i, p] = 1
     return pob
+
+def vector_per(dim , cant):
+    pob = []
+    pos = []
+    for i in range(0,dim):
+        pos.append(i)
+    while len(pob)!=50:
+        indiv=[]
+        for j in range(0, dim):
+            valor=random.choice(pos)
+            pos.remove(valor)
+            indiv.append(valor)
+        for i in range(0, dim):
+            pos.append(i)
+        pob.append(indiv)
+
+    return pob
+
+
+
+
+
+
+
+
