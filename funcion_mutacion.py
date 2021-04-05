2853c5771fe423c2f0f3e95da44c5f59c433050b
J0su3 <zS17013400@estudiantes.uv.com>
2021-04-04 23:10:26 -0500

    Se realizo la funcion de mutacion en la cual se toman dos elementos aleatoriamente en el individuo a estudiar y se intercambian estos elementos en el vectos

---
+++ funcion_mutacion.py
@@ -0,0 +1,11 @@
+import random
+
+def mutacion(indiv):
+    alea_1=random.randint(0,len(indiv)-1)
+    alea_2 = random.randint(0, len(indiv)-1)
+    interc_1=indiv[alea_1]
+    interc_2=indiv[alea_2]
+    indiv[alea_1] = interc_2
+    indiv[alea_2] = interc_1
+
+    return indiv
