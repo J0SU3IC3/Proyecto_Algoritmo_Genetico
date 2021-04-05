7ec5f7d02e9307e9de08df71da29c8084a665b5c
J0su3 <zS17013400@estudiantes.uv.com>
2021-04-05 11:34:23 -0500

    Se hizo la funcion de remplazo consiste en general de agrupar los elementos de las poblaciones en una sola lista, de los cuales se elge solo a aquellos con mejor fitnes, de tal suerte que el producto sera una lista con aquellos individios con mejor fitnes empezando con aquel con menor a mayor fitnes

---
+++ funcion_reemplazo.py
@@ -0,0 +1,25 @@
+from Funcion_Aptitud import aptfcn
+from funcion_cruza import cruza
+def reemplazo(gen_padres, desc):
+    poblacion = []
+    for i in range(len(gen_padres)):
+        indiv = gen_padres[i]
+        poblacion.append(indiv)
+    print(poblacion)
+    for i in range(len(desc)):
+        indiv = desc[i]
+        poblacion.append(indiv)
+    print(poblacion)
+    fitnes_pob = []
+    for i in range(len(gen_padres)+len(desc)):
+        fit_indiv = aptfcn(poblacion[i])
+        fitnes_pob.append(fit_indiv)
+    print(fitnes_pob)
+    elit_pob = []
+    while len(elit_pob)<len(gen_padres):
+        orden = min(fitnes_pob)
+        elit = poblacion.pop(fitnes_pob.index(orden))
+        elit_pob.append(elit)
+    return elit_pob
+
+
