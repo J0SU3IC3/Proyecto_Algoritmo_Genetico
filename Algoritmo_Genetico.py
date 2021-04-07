bed2662b647cbd69af15ad1e0661b81786eea004
J0su3 <zS17013400@estudiantes.uv.com>
2021-04-07 18:34:11 -0500

    se finalizo en un 90% el proyecto, ahra solo queda hacer pequeños ajustees meramente esteticos, pero a grandes rasgos todo esta hecho

--- Algoritmo_Genetico.py
+++ Algoritmo_Genetico.py
@@ -6,9 +6,10 @@ from funcion_mutacion import mutacion
 from funcion_reemplazo import reemplazo
 import numpy as np
 import pandas as pd
+import time
+import matplotlib.pyplot as plt
 
 print('¡Hola!,se bienvenido a mi algoritmo genetico')
-
 input("Pulsa enter para continuar...")
 
 print('Primero generaremos una población aleatoria de individuos (generación) que representan tableros de ajedrez')
@@ -23,15 +24,22 @@ print('\n')
 
 cantidad=int(input('¿Cuantos individuos quieres que tenga tu poblacion original?'))
 
-pob_ori = vector_per(8, cantidad)
+pob_ori_mem = vector_per(8, cantidad)
+pob_ori = pob_ori_mem
+
 
 gnss=int(input('¿Cuantas generaciones quieres observar?'))
 
 
 mejor_ind_gen = []
+mejores_fitnes_gen=[]
 peor_ind_gen = []
+peores_fitness_gen = []
 ind_prom_gen = []
+fitnes_prom_gen=[]
 desv_est = []
+generaciones = []
+
 for i in range(gnss):
     #seleccion de padres
     cndts, cndts_par = parent_slec(pob_ori)
@@ -48,6 +56,7 @@ for i in range(gnss):
     #reemplazo
     pob_fin = reemplazo(cndts, descn)
     pob_ori = pob_fin
+    generaciones.append(pob_ori)
 
     #individuos con mejor fitnes por generacion
     fitness_pob=[]
@@ -56,7 +65,7 @@ for i in range(gnss):
         indiv = pob_ori[i]
         fitness_pob.append(aptfcn(indiv))
     minfit = min(fitness_pob)
-
+    mejores_fitnes_gen.append(minfit)
     fittest_ind = []
     for j in range(len(pob_ori)):
         if minfit == fitness_pob[j]:
@@ -65,6 +74,7 @@ for i in range(gnss):
 
     #individuos con peor fitnes por geeneracion
     maxfit = max(fitness_pob)
+    peores_fitness_gen.append(maxfit)
 
     men_fittest_ind = []
     for j in range(len(pob_ori)):
@@ -73,7 +83,7 @@ for i in range(gnss):
     peor_ind_gen.append(men_fittest_ind)
     #individuos promedio
     promedio = int(sum(fitness_pob)/len(fitness_pob))
-
+    fitnes_prom_gen.append(sum(fitness_pob)/len(fitness_pob))
     ind_prom = []
     for i in range(len(fitness_pob)):
         if promedio == fitness_pob[i]:
@@ -88,16 +98,103 @@ for i in range(gnss):
     desv_est.append(np.sqrt(varianza))
 
 #tabla de algoritmo genetico
-tab1 = pd.DataFrame({mejor_ind_gen, peor_ind_gen, ind_prom_gen, desv_est },
-                    columns = ['Mejores individuos', 'Peores individuos', 'Individuos promedio','Desv. Est.' ])
+tab1 = pd.DataFrame({'Mejor Fitnes': mejores_fitnes_gen, 'Peor fitnes': peores_fitness_gen, 'Fitnes promedio': fitnes_prom_gen, 'Desv. Estandar':desv_est},
+                    columns=['Mejor Fitnes', 'Peor fitnes', 'Fitnes promedio', 'Desv. Estandar'])
+
+print('La siguiente tabla te muestra como se han desarrollado las poblaciones bajo los parametros que otorgaste')
+print(tab1)
+time.sleep(4)
+input('Presiona Enter para continuar')
+
+Resp=input('¿Te gustaria observar alguna generacion? \n [S/N]' )
+
+while Resp=='S':
+    original = input('¿Quieres ver la poblacion original?[S/N]')
+    if original == 'S':
+        for i in range(len(pob_ori_mem)):
+            print(pob_ori_mem[i], '\n')
+        fitnes_pob=[]
+        for i in range(len(pob_ori_mem)):
+            fitnes_pob.append(aptfcn(pob_ori_mem[i]))
+        minval=min(fitnes_pob)
+        min_fit=[]
+        for i in range(len(pob_ori_mem)):
+            if minval == fitnes_pob[i]:
+                min_fit.append(pob_ori_mem[i])
+        maxval=max(fitnes_pob)
+        max_fit = []
+        for i in range(len(pob_ori_mem)):
+            if maxval == fitnes_pob[i]:
+                max_fit.append(pob_ori_mem[i])
+        promen=sum(fitnes_pob)/len(pob_ori_mem)
+
+        print('Esta generacion tiene a los elementos')
+        for j in range(len(pob_ori_mem)):
+            print(pob_ori_mem[j], '\n')
+        print('Sus mejores individuos tienen un fitness de', minval, 'y estos son')
+        for j in range(len(min_fit)):
+            print(min_fit[j])
+        print('Sus peores individuos tienen un fitness de', maxval, 'y estos son')
+        for j in range(len(max_fit)):
+            print(max_fit[j])
+        print('En promedio la poblacion original tiene un fitnes de', promen)
+
+    obs_gen= input('¿Quieres ver alguna otra generacion?[S/N]')
+    if obs_gen == 'S':
+        pos=int(input('¿Que generacion quieres observar? Las generaciones estan enumeradas de 0 en adelante'))
+
+        observable=generaciones[pos]
+        print('Esta generacion tiene a los elementos')
+        for j in range(len(observable)):
+            print(observable[j], '\n')
+        print('Sus mejores individuos tienen un fitness de', mejores_fitnes_gen[pos], 'y estos son')
+        best=mejor_ind_gen[pos]
+        for j in range(len(best)):
+            print(best[j])
+        print('Sus peores individuos tienen un fitness de', peores_fitness_gen[pos], 'y estos son')
+        worst = mejor_ind_gen[pos]
+        for j in range(len(worst)):
+            print(worst[j])
+        print('En promedio esta generacion tiene un fitnes de', fitnes_prom_gen[pos])
+    Resp = input('¿Quieres ver alguna otra generacion?[S/N]')
+
+fitnes_pob=[]
+for i in range(len(pob_ori_mem)):
+    fitnes_pob.append(aptfcn(pob_ori_mem[i]))
+minval=min(fitnes_pob)
+min_fit=[]
+
+for i in range(len(pob_ori_mem)):
+    if minval == fitnes_pob[i]:
+        min_fit.append(pob_ori_mem[i])
+
+longitudes = []
+longitudes.append(len(min_fit))
+for i in range(len(mejor_ind_gen)):
+    longitudes.append(len(mejor_ind_gen[i]))
+
+print('la siguiente grafica muestra como se comportan las poblaciones a medida que pasa el tiempo \n'
+      'es decir, muestra cuantos individuos tienen un buen fitness mientras mas pasa el tiempo ')
+ejex = []
+for i in range(len(mejor_ind_gen)+1):
+    ejex.append(i)
+
+plt.plot(ejex,longitudes)
+plt.ylabel('Individuos mas aptos por generacion')
+plt.xlabel('Generacion (tiempo)')
+plt.show()
 
 
-
-
-
-
-
-
-
+promedios_n = []
+promedios_n.append(sum(fitnes_pob)/len(fitnes_pob))
+for i in range(len(fitnes_prom_gen)):
+    promedios_n.append(fitnes_prom_gen[i])
+print('tambien se puede notar que a medida que pase el tiempo el promedio de fitnes en la poblacion va descendiendo \n'
+      'esto quiere decir que mientras mas pase el tiempo futuras generaciones seran cada vez mas aptas dados \n'
+      'los parametros establecidos')
+plt.plot(ejex,promedios_n)
+plt.ylabel('Promedio por generacion')
+plt.xlabel('Generacion (tiempo)')
+plt.show()
 
 
